443e48da322625ea66a15143e993bcec5c116f04
Marden145 <moragafallas@gmail.com>
2026-09-18 00:01:17 -0600

    listo los controllers de album

---
+++ ApiCloudMedia/API/Controllers/AlbumController.cs
@@ -0,0 +1,32 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Services;
+using Abstracciones.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : Controller, IAlbumController
+    {
+        private readonly IAlbumServices _albumServices;
+        public AlbumController(IAlbumServices albumServices)
+        {
+            _albumServices = albumServices;
+        }
+
+        public async Task<IActionResult> AddAlbum(AlbumRequest albumRequest)
+        {
+            if(albumRequest==null)
+                return BadRequest("Invalid album request");
+            return Ok(await _albumServices.AddAlbum(albumRequest));
+        }
+
+        public async Task<IActionResult> AddAlbumMedia(AlbumMediaRequest albumMediaRequest)
+        {
+            if(albumMediaRequest==null)
+                return BadRequest("Invalid album media request");
+            return Ok(await _albumServices.AddAlbumMedia(albumMediaRequest));
+        }
+    }
+}
